4598368ffb50b87504f8d36778df32bb73d8d1c4
Maurits van der Schee <maurits@vdschee.nl>
2025-01-15 22:07:00 +0100

    add test structure

--- tests/serverTest.php
+++ tests/serverTest.php
@@ -2,20 +2,20 @@
 
 declare(strict_types=1);
 
-// use OpenSwoole\Server;
-// use PHPUnit\Framework\TestCase;
+use OpenSwoole\Server;
+use PHPUnit\Framework\TestCase;
 
-//define('TESTING', 1);
+define('TESTING', 1);
 //include 'server.php';
 
-// final class serverTest extends TestCase
-// {
-//     public function testGreetsWithName(): void
-//     {
-//         $greeter = new Server();
+final class serverTest extends TestCase
+{
+    public function testGreetsWithName(): void
+    {
+        $greeter = new Server();
 
-//         $greeting = $greeter->greet('Alice');
+        $greeting = $greeter->greet('Alice');
 
-//         $this->assertSame('Hello, Alice!', $greeting);
-//     }
-// }
+        $this->assertSame('Hello, Alice!', $greeting);
+    }
+}
